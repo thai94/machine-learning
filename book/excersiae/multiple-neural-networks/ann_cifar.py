1f21fb939cabd3ad72cffc816dd2b948831425e7
thainq <thainq@vng.com.vn>
2021-03-12 16:02:07 +0700

    udp

---
+++ book/excersiae/multiple-neural-networks/ann_cifar.py
@@ -0,0 +1,208 @@
+from random import randrange
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from random import random
+from math import exp
+
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    
+    network = []
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+
+    return network
+
+def activate(weights, inputs):
+    activation = weights[-1]
+    for i in range(len(weights)-1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = []
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+            print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+def predict(network, row):
+    outputs = forward_propagate(network, row)
+    return outputs.index(max(outputs))
+
+def ann(train, test, l_rate, n_epoch, n_hidden):
+	n_inputs = len(train[0]) - 1
+	n_outputs = len(set([row[-1] for row in train]))
+	network = initialize_network(n_inputs, n_hidden, n_outputs)
+	train_network(network, train, l_rate, n_epoch, n_outputs)
+	predictions = []
+	for row in test:
+		prediction = predict(network, row)
+		predictions.append(prediction)
+	return(predictions)
+
+def cross_validation_split(dataset, n_folds):
+    dataset_split = []
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = []
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
+
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            # row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def load_cifar_10_data(data_dir):
+    """
+    Return train_data, train_filenames, train_labels, test_data, test_filenames, test_labels
+    """
+
+    meta_data_dict = unpickle(data_dir + "/batches.meta")
+    cifar_label_names = meta_data_dict[b'label_names']
+    cifar_label_names = np.array(cifar_label_names)
+
+    # training data
+    cifar_train_data = None
+    cifar_train_filenames = []
+    cifar_train_labels = []
+
+    for i in range(1, 6):
+        cifar_train_data_dict = unpickle(data_dir + "/data_batch_{}".format(i))
+        if i == 1:
+            cifar_train_data = cifar_train_data_dict[b'data']
+        else:
+            cifar_train_data = np.vstack((cifar_train_data, cifar_train_data_dict[b'data']))
+        cifar_train_filenames += cifar_train_data_dict[b'filenames']
+        cifar_train_labels += cifar_train_data_dict[b'labels']
+
+    cifar_train_data = cifar_train_data.reshape((len(cifar_train_data), 3, 32, 32))
+    # ifar_train_data = np.rollaxis(cifar_train_data, 1, 4)
+    # cifar_train_data = cifar_train_data.transpose(0, 2, 3, 1).astype(np.float32)
+
+    cifar_train_filenames = np.array(cifar_train_filenames)
+    cifar_train_labels = np.array(cifar_train_labels)
+
+    cifar_test_data_dict = unpickle(data_dir + "/test_batch")
+    cifar_test_data = cifar_test_data_dict[b'data']
+    cifar_test_filenames = cifar_test_data_dict[b'filenames']
+    cifar_test_labels = cifar_test_data_dict[b'labels']
+
+    cifar_test_data = cifar_test_data.reshape((len(cifar_test_data), 3, 32, 32))
+    # cifar_test_data = np.rollaxis(cifar_test_data, 1, 4)
+    cifar_test_filenames = np.array(cifar_test_filenames)
+    cifar_test_labels = np.array(cifar_test_labels)
+
+    return cifar_train_data, cifar_train_filenames, cifar_train_labels, \
+        cifar_test_data, cifar_test_filenames, cifar_test_labels, cifar_label_names
+
+if __name__ == "__main__":
+    cifar_10_dir = 'cifar-10-batches-py'
+    train_data, train_filenames, train_labels, test_data, test_filenames, test_labels, label_names = \
+        load_cifar_10_data(cifar_10_dir)
+
+    print("Train data: ", train_data.shape)
+    print("Train filenames: ", train_filenames.shape)
+    print("Train labels: ", train_labels.shape)
+    print("Test data: ", test_data.shape)
+    print("Test filenames: ", test_filenames.shape)
+    print("Test labels: ", test_labels.shape)
+    print("Label names: ", label_names.shape)
+
+    # Vectorization
+    train_data_set = train_data.reshape(train_data.shape[0], 32 * 32 * 3)
+    lable = train_labels.reshape(train_labels.shape[0], 1)
+    train_data_set = np.concatenate((train_data_set, lable), axis = 1)
+
+    l_rate = 0.3
+    n_epoch = 500
+    n_hidden = 10
+    n_folds = 2
+
+    scores =  evaluate_algorithm(train_data_set.tolist()[0:1000], ann, n_folds, l_rate, n_epoch, n_hidden)
+    print('Scores: %s' % scores)
+    print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
